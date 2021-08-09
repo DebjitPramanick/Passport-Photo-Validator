f799f7b127c64ba15644c47afeef9a77ccefce39
DebjitPramanick <dpramanick9990@gmail.com>
2021-08-09 09:11:06 +0530

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import "./App.css"
 
 const App = () => {
+
+    const [img, setImg] = useState(null)
+
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0]
+        var reader = new FileReader();
+
+        reader.onloadend = function () {
+            setImg(reader.result)
+        }
+        reader.readAsDataURL(file);
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
     return (
         <div className="container">
-            
+            <div {...getRootProps()} className="drop-zone">
+                <input {...getInputProps()} />
+                {
+                    isDragActive ?
+                        <p>Drop the files here ...</p> :
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                }
+            </div>
+            <button>Upload</button>
+
+            {img && (
+                <div className="result">
+                    <div className="img-container">
+                        <img src={img} alt="/" />
+                    </div>
+                    <div className="text">
+                        This image is not passport.
+                    </div>
+                </div>
+            )}
+
         </div>
     )
 }
